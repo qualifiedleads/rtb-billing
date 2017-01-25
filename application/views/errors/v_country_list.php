94ade544fa38a3556bcabac7850b3486f36d20ca
mikertb <mike@rtb.cat>
2017-01-25 21:17:48 +0800

    Wrong files removed.

--- application/views/errors/v_country_list.php
+++
@@ -1,5 +0,0 @@
-<ul>
-<?php foreach($countries as $country):?>
-      <li data-code="<?php echo $country['code'];?>" onclick="countries.select(this)"><?php echo $country['name'];?></li>
-<?php endforeach; ?>
-</ul>
